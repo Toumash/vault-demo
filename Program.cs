e972c44ca4a52346bad706f1866d19d682daa1c5
Tomasz Dłuski <mr.toumash@gmail.com>
2019-05-25 16:08:54 +0200

    adds initial project with vault, log4net and configuration nugets

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.Commons;
+using VaultSharp.V1.SecretsEngines.Consul;
+
+namespace vaultsharp_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables().Build();
+
+            var vaultUrl = cfg.GetSection("vault-url").Value;
+            if (string.IsNullOrEmpty(vaultUrl))
+                throw new ArgumentNullException("vault-url not provided in the appsettings!");
+
+            var vaultToken = cfg.GetSection("vault-url").Value;
+
+            if (string.IsNullOrEmpty(vaultToken))
+                throw new ArgumentNullException("vault-token not provided in the appsettings!");
+
+            // Initialize one of the several auth methods.
+            var authMethod = new TokenAuthMethodInfo(vaultToken);
+
+            // Initialize settings. You can also set proxies, custom delegates etc. here.
+            var vaultClientSettings = new VaultClientSettings(vaultUrl, authMethod);
+
+            IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+
+            Secret<SecretData> kv2Secret = vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync("sample-secret").Result;
+            var login = kv2Secret.Data.Data["login"];
+            var password = kv2Secret.Data.Data["password"];
+            Console.WriteLine($"data from vault.\nLogin:{login} \nPassword{password}");
+        }
+    }
+}
